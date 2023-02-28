2f0345ceff730ca2c4799fadd63c0f3185a0c095
aryanibos <aryaisnaidi01@gmail.com>
2023-02-28 15:16:24 +0700

    PHP Dasar 2

---
+++ Pertemuan02/tugas/form_nilai.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Nilai Siswa</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="mt-5 mb-5">
+            <h2>Form Nilai Siswa</h2>
+        </div>
+        <form action="proses_nilai.php" method="get">
+            <div class="form-group row">
+                <label for="text1" class="col-4 col-form-label">Nama</label>
+                <div class="col-8">
+                    <input id="text1" name="nama" placeholder="Masukkan Nama" type="text" class="form-control"
+                        required="required">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="matkul" class="col-4 col-form-label">Mata kuliah</label>
+                <div class="col-8">
+                    <select id="matkul" name="matkul" class="custom-select" required="required">
+                        <option value="pemweb">Pemrograman Web</option>
+                        <option value="ddp">Dasar Dasar Pemrograman</option>
+                        <option value="bsd">Basis Data</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="uts" class="col-4 col-form-label">Nilai UTS</label>
+                <div class="col-8">
+                    <input id="uts" name="uts" placeholder="Masukkan Nilai UTS" type="text" class="form-control"
+                        required="required">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="uas" class="col-4 col-form-label">Nilai UAS</label>
+                <div class="col-8">
+                    <input id="uas" name="uas" placeholder="Masukkan Nilai UAS" type="text" class="form-control"
+                        required="required">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="pratikum" class="col-4 col-form-label">Nilai Tugas/Pratikum</label>
+                <div class="col-8">
+                    <input id="pratikum" name="pratikum" placeholder="Masukkan Nilai TUgas/Pratikum" type="text"
+                        class="form-control" required="required">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="offset-4 col-8">
+                    <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ Pertemuan02/tugas/proses_nilai.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_GET['submit'])) {
+    $nama = $_GET['nama'];
+    $matkul = $_GET['matkul'];
+    $uts = $_GET['uts'];
+    $uas = $_GET['uas'];
+    $pratikum = $_GET['pratikum'];
+    
+    echo "Nama : $nama <br>";
+    echo "Mata Kuliah : $matkul <br>";
+    echo "Nilai UTS : $uts <br>";
+    echo "Nilai UAS : $uas <br>";
+    echo "Nilai Tugas/Pratikum : $pratikum";
+}else{
+    echo "Tidak Ada Data";
+}
