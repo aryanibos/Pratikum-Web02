6a7bd64fcb03260ccbe5b9d9edf0a8119ad8a930
aryanibos <aryaisnaidi01@gmail.com>
2023-03-14 11:44:44 +0700

    Control Structure dan Function

--- Pertemuan03/proses_register.php
+++ Pertemuan03/proses_register.php
@@ -43,20 +43,38 @@ if (isset($_POST['submit'])) {
         $hasil = 0;
 
         foreach ($skills as $skill) {
-            $hasil = $hasil + $skill_list[$skill];
+            $hasil += $skill_list[$skill];
+        }
+
+        switch ($hasil) {
+            case ($hasil >= 100 && $hasil <= 150):
+                $hasil = "Sangat Baik";
+                break;
+            case ($hasil >= 60 && $hasil <= 99):
+                $hasil = "Baik";
+                break;
+            case ($hasil >= 40 && $hasil <= 59):
+                $hasil = "Cukup";
+                break;
+            case ($hasil >= 0 && $hasil <= 39):
+                $hasil = "Kurang";
+                break;
+            default:
+                $hasil = "Tidak Memandai";
+                break;
         }
         
-        if ($hasil >= 100 ) {
-            $hasil = "Sangat Baik";
-        } elseif ($hasil >= 60) {
-            $hasil = "Baik";
-        } elseif ($hasil >= 40) {
-            $hasil = "Cukup";
-        } elseif ($hasil >= 0) {
-            $hasil = "Kurang";
-        } else {
-            $hasil = "Tidak Memandai";
-        }
+        // if ($hasil >= 100 ) {
+        //     $hasil = "Sangat Baik";
+        // } elseif ($hasil >= 60) {
+        //     $hasil = "Baik";
+        // } elseif ($hasil >= 40) {
+        //     $hasil = "Cukup";
+        // } elseif ($hasil >= 0) {
+        //     $hasil = "Kurang";
+        // } else {
+        //     $hasil = "Tidak Memandai";
+        // }
         
         return $hasil;
     }
